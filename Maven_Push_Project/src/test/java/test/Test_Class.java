7f3f31a790f033b06ac0abf049b3b3b7e6a17724
admodi <admodi@ADMODI-LAP.oradev.oraclecorp.com>
2019-07-07 02:59:47 +0530

    Test new branch

--- Maven_Push_Project/src/test/java/test/Test_Class.java
+++ Maven_Push_Project/src/test/java/test/Test_Class.java
@@ -5,7 +5,7 @@ public class Test_Class {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello Wor");
+		System.out.println("Hello Wr");
 		
 		
 	}
